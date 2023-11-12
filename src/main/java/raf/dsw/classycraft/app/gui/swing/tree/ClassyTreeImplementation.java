d952391a678fa7d24bbe41c0224f69bae1d566bf
Vlajkovic11 <andrija.mv@gmail.com>
2023-11-12 19:12:37 +0100

    komit treeimp

--- src/main/java/raf/dsw/classycraft/app/gui/swing/tree/ClassyTreeImplementation.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/tree/ClassyTreeImplementation.java
@@ -6,6 +6,7 @@ import raf.dsw.classycraft.app.factory.FactoryAbstract;
 import raf.dsw.classycraft.app.factory.FactoryUtils;
 import raf.dsw.classycraft.app.gui.swing.tree.model.ClassyTreeItem;
 import raf.dsw.classycraft.app.gui.swing.tree.view.ClassyTreeView;
+import raf.dsw.classycraft.app.gui.swing.view.MainFrame;
 import raf.dsw.classycraft.app.repository.composite.ClassyNode;
 import raf.dsw.classycraft.app.repository.composite.ClassyNodeComposite;
 import raf.dsw.classycraft.app.repository.implementation.Package;
@@ -14,6 +15,10 @@ import raf.dsw.classycraft.app.repository.implementation.ProjectExplorer;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import static raf.dsw.classycraft.app.factory.FactoryPackage.getI;
 
@@ -27,6 +32,7 @@ public class ClassyTreeImplementation implements ClassyTree{
         ClassyTreeItem root = new ClassyTreeItem(projectExplorer);
         treeModel = new DefaultTreeModel(root);
         treeView = new ClassyTreeView(treeModel);
+        setupMouseListener();
         return treeView;
     }
 
@@ -110,5 +116,21 @@ public class ClassyTreeImplementation implements ClassyTree{
             return new Package(packageName, parent);
         }
         return null;
+    }
+    public void setupMouseListener() {
+        treeView.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    TreePath path = treeView.getSelectionPath();
+                    if (path != null && path.getLastPathComponent() instanceof ClassyTreeItem) {
+                        ClassyTreeItem item = (ClassyTreeItem) path.getLastPathComponent();
+                        if (item.getClassyNode() instanceof Package) {
+                            Package pkg = (Package) item.getClassyNode();
+                            MainFrame.getInstance().getPackageView().subscribeToPackage(pkg);
+                        }
+                    }
+                }
+            }
+        });
     }
 }
